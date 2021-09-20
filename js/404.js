e2e78286a0f68050e3af3102fa475402e1d7fdea
Brian Gotti <40281268+bgotti@users.noreply.github.com>
2021-09-20 12:57:01 -0400

    Add files via upload

---
+++ js/404.js
@@ -0,0 +1,19 @@
+//404 Error
+
+setTimeout(function(){ window.location="index"; },3000);
+
+
+//Menu Hamburger
+
+const menuSlider = () => {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+
+  hamburger.addEventListener('click', () => {
+    menu.classList.toggle('menu__active')
+    hamburger.classList.toggle('toggle');
+  });
+
+}
+
+menuSlider();
